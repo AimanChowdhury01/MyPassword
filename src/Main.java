c15e1ed1fdef19e48a64a719c28b787541526bc7
Aiman <aimannawar.chowdhury@gmail.com>
2025-04-12 17:33:42 -0600

    final

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to My Passwords !!!");
-        while (true) {
+        while (true) {//starts loop for create and login
             System.out.print("Press 'C' to Create account and 'L' to Login: ");
             String choice = scanner.nextLine();
             if (choice.equalsIgnoreCase("C")) {
@@ -15,7 +15,7 @@ public class Main {
                 break;
             }
             else {
-                System.out.println("Invalid input. Please enter 'C' or 'L'.");
+                System.out.println("Invalid input. Please enter 'C' or 'L'.");//redirects to start of loop
             }
         }
         scanner.close();
